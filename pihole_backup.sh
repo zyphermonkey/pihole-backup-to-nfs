1294ebb362524c48f846663771f2dcd04bd7e903
zyphermonkey <zyphermonkey@gmail.com>
2023-09-10 05:25:35 -0400

    add remount and tar fixes
    tar fixes are needed because `--remove-files` wasn't actually cleaning up files because the files were still in use therefore leaving behind files.
    
    The new tar commands separate out the remove files into individual runs.

--- pihole_backup.sh
+++ pihole_backup.sh
@@ -7,13 +7,19 @@
 # mv /home/pi/pihole-$(date '+%F').tar.gz /mnt/Backup/
 # find /mnt/Backup -name "pihole-*.tar.gz" -type f -mtime +7 -exec rm -f {} \;
 
-# v2 
-# Do all work remotely. 
-# Consumes more bandwidth and takes longer, but avoids needing extra local storage.
+# v2 work remotely. Consumes more bandwidth, but avoids needing extra local storage.
+
+# NFS mount goes stale and needs to be remounted. Doing this before each backup to ensure mount is available.
+sudo umount -f /mnt/Backup
+sudo mount /mnt/Backup
+
 mkdir /mnt/Backup/pihole-$(date '+%F')
 cd /mnt/Backup/pihole-$(date '+%F')
 pihole -a -t
 sqlite3 /etc/pihole/pihole-FTL.db ".backup /mnt/Backup/pihole-$(date '+%F')/pihole-FTL.db.backup"
-tar -czvf /mnt/Backup/pihole-$(date '+%F').tar.gz /mnt/Backup/pihole-$(date '+%F') --remove-files
+# tar -czvf /mnt/Backup/pihole-$(date '+%F').tar.gz /mnt/Backup/pihole-$(date '+%F') --remove-files
+tar -czvf /mnt/Backup/pihole-$(date '+%F').tar.gz /mnt/Backup/pihole-$(date '+%F')
+rm -rf /mnt/Backup/pihole-$(date '+%F') 2&>/dev/null
+rm -rf /mnt/Backup/pihole-$(date '+%F')
 # mv /home/pi/pihole-$(date '+%F').tar.gz /mnt/Backup/
 find /mnt/Backup -name "pihole-*.tar.gz" -type f -mtime +7 -exec rm -f {} \;
